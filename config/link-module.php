d71519b5dadd70617465989818c5b510494d7c02
Hayley Berry <hayley.berry@appreciategroup.co.uk>
2024-10-17 17:06:43 +0100

    Cache OAuth token from AWS Cognito and pass to service

--- config/link-module.php
+++ config/link-module.php
@@ -26,4 +26,24 @@ return [
 
     'verify_ssl' => env('LINK_MODULE_VERIFY_SSL', env('VERIFY_SSL', true)),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication
+    |--------------------------------------------------------------------------
+    |
+    | Credentials for Cognito authentication.
+    |
+    */
+
+    'auth' => [
+        'token_url' => env('LINK_MODULE_AUTH_TOKEN_URL'),
+        'client_id' => env('LINK_MODULE_AUTH_CLIENT_ID'),
+        'client_secret' => env('LINK_MODULE_AUTH_CLIENT_SECRET'),
+
+        'token_cache_key' => env(
+            'LINK_MODULE_AUTH_TOKEN_CACHE_KEY',
+            ((string) env('APP_NAME')) . ((string) env('APP_ENV')) . '-link-module-auth-token'
+        ),
+    ]
+
 ];
--- src/LinkModuleServiceProvider.php
+++ src/LinkModuleServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace MBLSolutions\LinkModuleLaravel;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
+use MBLSolutions\LinkModule\Api\OAuthResource;
 use MBLSolutions\LinkModule\Auth\LinkModule;
 use MBLSolutions\LinkModule\Links;
 
@@ -25,6 +27,26 @@ class LinkModuleServiceProvider extends ServiceProvider
         LinkModule::setVerifySSL($this->app['config']['link-module.verify_ssl']);
 
         $this->app->singleton(LinkModuleService::class, function () {
+            $cacheHit = Cache::get($this->app['config']['link-module.auth.token_cache_key']);
+
+            if (! $cacheHit) {
+                $authClient = new OAuthResource(
+                    tokenUri: $this->app['config']['link-module.auth.token_url'],
+                    clientId: $this->app['config']['link-module.auth.client_id'],
+                    clientSecret: $this->app['config']['link-module.auth.client_secret']
+                );
+
+                $accessToken = $authClient->getToken();
+
+                $fullTokenString = $accessToken->tokenType . ' ' . $accessToken->accessToken;
+
+                Cache::set($this->app['config']['link-module.auth.token_cache_key'], $fullTokenString, $accessToken->expiresIn - 60);
+            } else {
+                $fullTokenString = $cacheHit;
+            }
+
+            LinkModule::setToken($fullTokenString);
+
             return new LinkModuleService(
                 linksClient: $this->app->make(Links::class),
             );
